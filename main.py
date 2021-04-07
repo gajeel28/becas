81ba8b7ab245bbb7146d5fe8c451fc3a1a53f19f
gajeel28 <70728110+gajeel28@users.noreply.github.com>
2021-04-07 15:53:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pandas as pd
 
-dron = pd.read_excel(r"G:\Trabajo\Listado_Examen_Becarias_GobCba_M02_DNT_ManejoDrones.xlsx")
-robotica = pd.read_excel(r"G:\Trabajo\Calificaciones_SEU_DNT_M03_IntroRob_Becarias.xlsx")
-programacion = pd.read_excel(r"G:\Trabajo\Calificaciones_M01_DNT_IntroProg_BecasGobCba_V01.xlsx")
-new_lista = pd.concat([dron, robotica, programacion])
+df_dron = pd.read_excel(r"C:\Users\20361433069\Desktop\becas\Listado Examen Becarias GobCba M02_DNT_ManejoDrones (1).xlsx")
+df_robotica = pd.read_excel(r"C:\Users\20361433069\Desktop\becas\Calificaciones SEU_DNT_M03_IntroRob_Becarias.xlsx")
+df_programacion = pd.read_excel(r"C:\Users\20361433069\Desktop\becas\Calificaciones M01_DNT_IntroProg_BecasGobCba_V01 (1).xlsx")
+
+df_dron['Beca'] = 'Manejo de dron'
+df_robotica['Beca'] = 'Robotica'
+df_programacion['Beca'] = 'Programacion'
+
+new_lista = pd.concat([df_dron, df_robotica, df_programacion])
 new_lista.to_excel("Nomina de becas.xlsx", index = False, sheet_name = "hoja1", header = 1)
 
 
@@ -35,12 +40,7 @@ new_lista.to_excel("Nomina de becas.xlsx", index = False, sheet_name = "hoja1", 
 
 
 
-
-
-
+#new_dron = pd.concat([df_dron, df_1], axis = 1, sort = False) # se agrega la columna beca en la lista de dron
+#new_robotica = pd.concat([df_robotica, df_2], axis = 1, sort = False)# se agrega la columna beca a la lista robo
+#new_programacion = pd.concat([df_programacion, df_3], axis = 1, sort = False) # se agrega beca a la lista programacion
 #df = pd.DataFrame(dron, columns = ["Beca"])
-#resultado = df.to_excel("prueba.xlsx")
-#todo = pd.concat([dron, robotica, programacion])
-#print(robotica)
-#print(progrmacion)
-#print(todo)
